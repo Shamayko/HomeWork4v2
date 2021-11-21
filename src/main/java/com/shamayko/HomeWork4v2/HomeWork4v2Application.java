7f884f2bbb984c25e0126d9bde978fef6f067771
Shamayko <shamayko@gmail.com>
2021-11-21 12:11:00 +0300

    Initial commit

---
+++ src/main/java/com/shamayko/HomeWork4v2/HomeWork4v2Application.java
@@ -0,0 +1,13 @@
+package com.shamayko.HomeWork4v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork4v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork4v2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shamayko/HomeWork4v2/HomeWork4v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamayko.HomeWork4v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork4v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
